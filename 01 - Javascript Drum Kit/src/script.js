8385a023ecdc6379af7601f57a9d6ea14c3ed3d1
seckin <contactseckin@gmail.com>
2024-04-24 01:41:05 +0300

    first commit

---
+++ 01 - Javascript Drum Kit/src/script.js
@@ -0,0 +1,42 @@
+const keyName ={
+    A : ("../assets/sounds/clap.wav"), 
+    S : ("../assets/sounds/hihat.wav"),
+    D : ("../assets/sounds/kick.wav"),
+    F : ("../assets/sounds/openhat.wav"),
+    G : ("../assets/sounds/boom.wav"),
+    H : ("../assets/sounds/ride.wav"),
+    J : ("../assets/sounds/snare.wav"),
+    K : ("../assets/sounds/tom.wav"),
+    L : ("../assets/sounds/tink.wav"),
+}
+
+
+let letters ; 
+
+// selectors should be used after contents loaded , otherwise it returns undefined . 
+window.addEventListener('DOMContentLoaded',() =>{
+    letters = document.querySelectorAll('[data-letter]');
+})
+
+
+document.addEventListener('keydown',(e)=>{
+    let audio = new Audio(keyName[e.key.toUpperCase()])  
+    audio.play();
+
+
+
+    letters.forEach(element => {
+        if(element.dataset.letter == e.key.toUpperCase())
+            element.classList.toggle("playingDiv")
+
+        
+    });
+
+})
+
+
+//remove playingDiv class when transitionend event happens
+document.addEventListener('transitionend',(e) =>{
+    e.target.classList.remove('playingDiv')
+})
+
