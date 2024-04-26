9191a41a6c10d6edefb87f4c8a4199a4835b6302
seckin <contactseckin@gmail.com>
2024-04-27 01:51:47 +0300

    Css Variables is released

--- 03 - Css Variables/src/script.js
+++ 03 - Css Variables/src/script.js
@@ -1,20 +1,18 @@
 
 
-let spacing; let blur; let baseColor;
-
-    spacing=  document.getElementById('spacing')
-    blur =  document.getElementById('blur')
-    baseColor = document.getElementById("baseColor");
-    
-    console.log(spacing);
-    console.log(blur);
 
 
 
+const root = document.documentElement;
+
+
+ function valueChanged(id,newValue){
+    let suffix = (id != 'baseColor' ? 'px' : '');
+    root.style.setProperty('--' + id , newValue + suffix),
+    console.log(newValue + suffix);
 
 
 
-spacing.addEventListener("input",(e)=>{
-    console.log(e)
-    spacing.style.setProperty(".control",e.target.value);
-})
+}
+
+
