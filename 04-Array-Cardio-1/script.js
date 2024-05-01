156b0971d24cea6ff8fc0c656fee60add986ae44
seckin <contactseckin@gmail.com>
2024-05-01 23:21:17 +0300

    array exercises made within another file , this is just to make reminder

---
+++ 04-Array-Cardio-1/script.js
@@ -0,0 +1,9 @@
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+const transportation = data.reduce(function(obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
