0e1403a6ed648326c576da77ecd40acdf6cd9624
seckin <contactseckin@gmail.com>
2024-05-06 02:21:38 +0300

    project 6 is done , overflow-x , map with regex is studied

--- 06-Ajax-Type-Ahead/src/script.js
+++ 06-Ajax-Type-Ahead/src/script.js
@@ -1,10 +1,7 @@
 
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
 const cities=[];
-
 const suggestions = document.querySelector('ul');
-
 
 const inputElement = document.querySelector('.search')
 inputElement.addEventListener('keyup',findMatchingSamples);
@@ -22,21 +19,28 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+function  findMatchingSamples2(cities,textValue){
+    return cities.filter((element) => {
+    const searchText = textValue.toLowerCase();
+    const cityMatch = element['city'].toLowerCase().includes(searchText);
+    const stateMatch = element['state'].toLowerCase().includes(searchText);
+    return cityMatch || stateMatch;
+  });
+}
 
 
 
 export function findMatchingSamples(){
-  let matchingCities = cities.filter((element) => {return element['city','state'].toLowerCase().includes(this.value)});
+  let matchingCities = findMatchingSamples2(cities,this.value)
 
-
-  const html = matchingCities.map(place => {
+  const html = matchingCities.map(element => {
     const regex = new RegExp(this.value, 'gi');
-    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    const cityName = element.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = element.state.replace(regex, `<span class="hl">${this.value}</span>`);
     return `
       <li>
         <span class="name">${cityName}, ${stateName}</span>
-        <span class="population">${numberWithCommas(place.population)}</span>
+        <span class="population">${numberWithCommas(element.population)}</span>
       </li>
     `;
   }).join('');
